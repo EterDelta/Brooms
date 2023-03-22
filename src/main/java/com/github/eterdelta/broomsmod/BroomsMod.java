04bb79f3ce8da59832d4cf22a760d3c6cf23de35
EterDelta <67644822+EterDelta@users.noreply.github.com>
2023-03-21 21:49:33 -0500

    Register client stuff in main class

--- src/main/java/com/github/eterdelta/broomsmod/BroomsMod.java
+++ src/main/java/com/github/eterdelta/broomsmod/BroomsMod.java
@@ -1,9 +1,12 @@
 package com.github.eterdelta.broomsmod;
 
+import com.github.eterdelta.broomsmod.client.renderer.WoodenBroomRenderer;
+import com.github.eterdelta.broomsmod.client.renderer.model.WoodenBroomModel;
 import com.github.eterdelta.broomsmod.registry.BroomsEnchantments;
 import com.github.eterdelta.broomsmod.registry.BroomsEntities;
 import com.github.eterdelta.broomsmod.registry.BroomsItems;
 import com.github.eterdelta.broomsmod.registry.BroomsSounds;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -19,5 +22,16 @@ public class BroomsMod {
         BroomsEntities.ENTITIES.register(eventBus);
         BroomsItems.ITEMS.register(eventBus);
         BroomsSounds.SOUND_EVENTS.register(eventBus);
+
+        eventBus.addListener(this::registerEntityRenderers);
+        eventBus.addListener(this::registerEntityLayers);
+    }
+
+    public void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(BroomsEntities.WOODEN_BROOM.get(), WoodenBroomRenderer::new);
+    }
+
+    public void registerEntityLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(WoodenBroomModel.LAYER_LOCATION, WoodenBroomModel::createBodyLayer);
     }
 }
